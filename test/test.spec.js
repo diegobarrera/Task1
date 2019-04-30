28f6289791ebede531db0be61112debeb8759b48
Diego Barrera <dieg_goo@hotmail.com>
2019-04-29 21:15:54 -0500

    implementation of the unit tests

--- test/test.spec.js
+++ test/test.spec.js
@@ -2,7 +2,7 @@ const { expect } = require('chai')
 const { solution, Child } = require('../src/index')
 
 describe('Test task 1', () => {
-  
+
   it('should return an array of objects', () => {
     let testArray = [1, 3, 4, 6];
     let result = solution(testArray)
@@ -23,17 +23,26 @@ describe('Test task 1', () => {
   })
 
   it('Should match the value() with the initial array', () => {
-    let testArray = [3, 2,78,1,9];
+    let testArray = [3, 2, 78, 1, 9];
     let result = solution(testArray)
-    expect(result[0].value === result[1].value).to.be.true
+    expect(result[0].value()).to.equal(3)
+    expect(result[1].value()).to.equal(2)
+    expect(result[2].value()).to.equal(78)
+    expect(result[3].value()).to.equal(1)
+    expect(result[4].value()).to.equal(9)
   })
 
-  it('Should work with an empty array', () =>{
-
+  it('Should work with an empty array', () => {
+    let testArray = [];
+    let result = solution(testArray)
+    expect(result).to.have.lengthOf(0)
   })
 
   it('Should work with big numbers', () => {
-
+    let testArray = [1, 2, 99999, 1000000, 100001];
+    let result = solution(testArray)
+    expect(result).to.have.lengthOf(5)
+    expect(result[4].value()).to.equal(100001)
   })
 
 })
